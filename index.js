cea1b39b9f9d231cffa451f301706435a7016e70
anickel101 <anson.nickel@gmail.com>
2020-09-17 15:53:40 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const superbowlWin = (array) => {
+  let query = array.find(object => object.result === "W");
+  return !!query ? query.year : undefined;
+  }
