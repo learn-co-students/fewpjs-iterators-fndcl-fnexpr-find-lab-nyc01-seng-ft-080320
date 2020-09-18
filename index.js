015faadc433fe75734c01246941df635379c830a
kevinpark07 <kevin.park07@yahoo.com>
2020-09-17 22:24:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-const testVar = {}
+// const testVar = {}
 
-function testFunc() {
-  return "hi"
+// function testFunc() {
+//   return "hi"
+// }
+
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
 }
+
+//MY CORRECT ANSWER!!
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "W"},
+//   {year: "2016", result: "N/A"}
+// ]
+
+// const superbowlWin = (array) => {
+  
+//   const winsArray = array.find(record => {
+//      if (record.result === "W") {
+//      return record.year
+//      }
+//   })
+//   return winsArray
+// }
+
+// superbowlWin(record);
