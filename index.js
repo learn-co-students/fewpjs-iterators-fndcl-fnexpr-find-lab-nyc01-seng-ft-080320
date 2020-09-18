496f5cd43b4827c3b60756edef0de6513da1e706
Mattie43 <ericksenm93@ymail.com>
2020-09-17 21:21:28 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let team = arr.find( obj => obj.result === 'W' )
+  if (team) { return team.year } else { team }
+}
