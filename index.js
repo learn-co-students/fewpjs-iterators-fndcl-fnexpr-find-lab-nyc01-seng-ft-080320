1f45594a08ec78d956e3fae663d8c19fe913958d
slurio <samantha.lurio@gmail.com>
2020-09-17 20:56:40 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(record) {
+  const result = record.find(function(season) {return season["result"] === "W"})
+  if(result) {
+    return result.year
+  }
+  else {
+    return undefined
+  }
+}
